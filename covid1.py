b31c21f60510cd4f5196a76d968cb1ce90f69fea
angelsaez251 <angelsaez251@gmail.com>
2021-10-25 08:53:21 +0200

    yes

---
+++ covid1.py
@@ -0,0 +1,10 @@
+
+import requests, json
+import urllib3
+
+
+
+url = "https://api.covid19tracking.narrativa.com/api/countries"
+response = requests.request("GET", url, verify=False)
+data = dict(json.loads(response.text))
+
---
+++ test.py
@@ -0,0 +1,4 @@
+def nameOfTheFunction(arg1="Default1", arg2="Default2"):
+    print("Hello")
+
+nameOfTheFunction()
